7ce02bafb9cfa7092d1158a54d9ca94eaa14ab0c
robinhultman <robin.hultman@gmail.com>
2014-10-07 08:11:51 +0200

    Added config repository for reading config values from sso store

---
+++ Src/SSOConfigRepository/SSOClientHelper.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Specialized;
+using Microsoft.BizTalk.SSOClient.Interop;
+
+namespace BizTalkComponents.Utils.SSOConfigRepository
+{
+   public class ConfigurationPropertyBag : IPropertyBag
+    {
+        private readonly HybridDictionary _properties;
+        internal ConfigurationPropertyBag()
+        {
+            _properties = new HybridDictionary();
+        }
+        public void Read(string propName, out object ptrVar, int errLog)
+        {
+            ptrVar = _properties[propName];
+        }
+        public void Write(string propName, ref object ptrVar)
+        {
+            _properties.Add(propName, ptrVar);
+        }
+        public bool Contains(string key)
+        {
+            return _properties.Contains(key);
+        }
+        public void Remove(string key)
+        {
+            _properties.Remove(key);
+        }
+    }
+
+    public static class SSOClientHelper
+    {
+        private const string idenifierGUID = "ConfigProperties";
+
+        /// <summary>
+        /// Read method helps get configuration data
+        /// </summary>        
+        /// <param name="appName">The name of the affiliate application to represent the configuration container to access</param>
+        /// <param name="propName">The property name to read</param>
+        /// <returns>
+        ///  The value of the property stored in the given affiliate application of this component.
+        /// </returns>
+        public static string Read(string appName, string propName)
+        {
+            try
+            {
+                var ssoStore = new SSOConfigStore();
+                var appMgmtBag = new ConfigurationPropertyBag();
+                ((ISSOConfigStore) ssoStore).GetConfigInfo(appName, idenifierGUID, SSOFlag.SSO_FLAG_RUNTIME, appMgmtBag);
+                object propertyValue;
+                appMgmtBag.Read(propName, out propertyValue, 0);
+                return (string) propertyValue;
+            }
+            catch (Exception e)
+            {
+                System.Diagnostics.Trace.WriteLine(e.Message);
+                throw;
+            }
+        }
+    }
+}
---
+++ Src/SSOConfigRepository/SSOConfigRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using BizTalkComponents.Utils.ConfigRepository;
+
+namespace BizTalkComponents.Utils.SSOConfigRepository
+{
+    public class SSOConfigRepository : IConfigRepository
+    {
+        public string ReadConfigValue(params string[] keys)
+        {
+            if (keys.Length < 2)
+            {
+                throw new ArgumentException("Number of keys is not correct. Expected 2");
+            }
+
+            return SSOClientHelper.Read(keys[0], keys[1]);
+        }
+    }
+}
