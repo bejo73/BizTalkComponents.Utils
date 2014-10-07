08aa08776a4bcbcb3ef21483412c5661c48e5b91
robinhultman <robin.hultman@gmail.com>
2014-10-07 08:28:20 +0200

    Added new config repository to read from appconfig files

---
+++ Src/AppConfigRepository/AppConfigRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Configuration;
+using BizTalkComponents.Utils.ConfigRepository;
+
+namespace BizTalkComponents.Utils.AppConfigRepository
+{
+    public class AppConfigRepository : IConfigRepository
+    {
+        public string ReadConfigValue(params string[] keys)
+        {
+            if (keys.Length < 1)
+            {
+                throw new ArgumentException("Number of keys is not correct. Expected 1");
+            }
+
+            return ConfigurationManager.AppSettings[keys[0]];
+        }
+    }
+}
